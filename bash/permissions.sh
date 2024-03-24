182995e50b962906281420cea16bf2dc3860ff7f
NicHub <nicolas.jeanmonod@gmail.com>
2024-03-24 10:55:37 +0100

    update

--- bash/permissions.sh
+++ bash/permissions.sh
@@ -5,5 +5,19 @@
 FILENAME=permissions.sh
 stat -f '%A' $FILENAME
 
-# chmod 644 $FILENAME
-# xattr -rc .
+# 644 = -rw-r--r--
+chmod 644 $FILENAME
+
+# Remove extended attributes
+xattr -rc .
+
+# Change file permissions recursively
+# 666 = -rw-rw-rw-
+sudo find . -type f \( -path "./.git/*" -prune -o -exec chmod 666 {} \; \)
+
+# Change directory permissions recursively
+# 777 = drwxrwxrwx
+sudo find . -type d \( -path "./.git/*" -prune -o -exec chmod 777 {} \; \)
+
+# Change owner recursively
+sudo chown -R $USER .
