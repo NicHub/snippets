964b0b3b0c55df7f2a78f05d0c24ce96d3689d3b
NicHub <nicolas.jeanmonod@gmail.com>
2025-12-02 14:00:08 +0100

    args

--- python/args.py
+++ python/args.py
@@ -1,10 +1,22 @@
-"""
-
-"""
+""" """
 
 import sys
+from rich import print
 
 if __name__ == "__main__":
 
     print(f"Number of arguments: {len(sys.argv)}")
     print(f"Argument List: {str(sys.argv)}")
+
+    print("**********")
+    print(sys.argv)
+    print("##########")
+
+    if len(sys.argv) > 1 and "Jupyter" in sys.argv[1]:
+        print("Running in Jupyter notebook")
+    else:
+        print("Running in Terminal")
+
+    # Other solution
+    if len(sys.argv) > 1 and "Jupyter" not in sys.argv[1]:
+        print("Running in Terminal")
