5f6f89a1756478ee7496aaa35e11890f4b6cb028
NicHub <nicolas.jeanmonod@gmail.com>
2026-09-06 16:21:50 +0200

    nokdrive_for_git

--- bash/nokdrive_for_git.sh
+++ bash/nokdrive_for_git.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# !!! WARNING This solution is not tolerant to `git clean -fdx` !!!
+
 CUR_GIT_DIR=".git"
 NEW_GIT_DIR=".git-nogit-nogitclean-nokdrive"
 mv "$CUR_GIT_DIR" "$NEW_GIT_DIR"
