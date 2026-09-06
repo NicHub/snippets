9ba5519fd4c4c36e964cf71120f9a28bdc73a691
NicHub <nicolas.jeanmonod@gmail.com>
2026-09-06 16:22:43 +0200

    nokdrive_for_git

--- bash/nokdrive_for_git.sh
+++ bash/nokdrive_for_git.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# https://chatgpt.com/share/6a9d7710-5ac4-83ed-b8a6-19b08e471a9e
+# https://chatgpt.com/s/task_7f71d83d281881919a8fa757b797c656
 # !!! WARNING This solution is not tolerant to `git clean -fdx` !!!
 
 CUR_GIT_DIR=".git"
